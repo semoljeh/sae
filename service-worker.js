0939b5be79093bc53fab63ced9536568424a39b7
semoljeh <aromtemplate@gmail.com>
2026-06-07 15:17:18 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Ganti nama cache untuk memaksa pembaruan secara instan
-const CACHE_NAME = "almukhtar-cache-v4"; // Versi saya naikkan ke v4 agar cache lama tertimpa
+const CACHE_NAME = "almukhtar-cache-v5"; // Versi naikkan agar cache lama tertimpa
 
 const LOCAL_ASSETS = [
   "./",
